6b4b92211a95c840ee45b99d8d190ba4d2897a77
Thiago Dornelas <thiagohmdornelas@gmail.com>
2025-11-14 15:51:39 -0300

    criação do app Reviews

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView #genre_create_list_view, genre_detail_view, 
 from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
 from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
+from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
 
 
 
@@ -21,5 +23,9 @@ urlpatterns = [
     
     # *** MOVIE *** 
     path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
-    path('movies/<int:pk>', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-list')
+    path('movies/<int:pk>', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-list'),
+
+    # *** REVIEW ***
+    path('reviews/', ReviewCreateListView.as_view(), name='review-create-list'),
+    path('reviews/<int:pk>', ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-list'),
 ]
